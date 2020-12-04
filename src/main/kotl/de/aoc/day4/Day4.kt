4ea85bf91a3aebcebe142ee5c4bed061d2a544db
Julia Burgard <burgard@synyx.de>
2020-12-04 17:09:42 +0100

    Add solution for day 4 b

--- src/main/kotl/de/aoc/day4/Day4.kt
+++ src/main/kotl/de/aoc/day4/Day4.kt
@@ -1,19 +1,95 @@
 package de.aoc.day4
 
-import java.util.regex.Pattern
-
 
 fun day4_a(inputAsString: String): Int {
-    return inputAsString.split("\n\n").map { Passport(it) }.filter { it.isValid() }.count();
+    return inputAsString.split("\n\n").map { Passport(it) }.filter { it.requiredFieldsSet() }.count();
 }
+
+fun day4_b(inputAsString: String): Int {
+    return inputAsString.split("\n\n").map { Passport(it) }.filter { it.allFieldsAreValid() }.count();
+}
+
 
 data class Passport(var input: String) {
-    val components = "(\\w*):".toRegex().findAll(input).map { it.destructured.component1() }.toList()
+    val byr = parse("byr")?.toInt()
+    val iyr = parse("iyr")?.toInt()
+    val eyr = parse("eyr")?.toInt()
+    val hgt = parse("hgt")?.let { Hgt(it) }
+    val hcl = parse("hcl")?.let { Hcl(it) }
+    val ecl = parse("ecl")?.let { Ecl(it) }
+    val pid = parse("pid")?.let { Pid(it) }
 
 
-    fun isValid(): Boolean {
-        return components.filter { it != "cid" }.count() == 7
+    fun requiredFieldsSet(): Boolean {
+        return byr != null && iyr != null && eyr != null && hgt != null && hcl != null && ecl != null && pid != null
     }
+
+    fun allFieldsAreValid(): Boolean {
+        val byrValid = IntRange(1920, 2002).contains(byr)
+        val iyrValid = IntRange(2010, 2020).contains(iyr)
+        val eyrValid = IntRange(2020, 2030).contains(eyr)
+        val hgtValid = hgt?.isValid() ?: run { false }
+        val eclValid = ecl?.isValid() ?: run { false }
+        val hclValid = hcl?.isValid() ?: run { false }
+        val pidValid = pid?.isValid() ?: run { false }
+        return byrValid && iyrValid && eyrValid && hgtValid && eclValid && hclValid && pidValid;
+
+    }
+
+    private fun parse(key: String): String? {
+        val find = "$key:(\\w*\\d*\\S*[^\\s])".toRegex().find(input)
+        if (find != null) {
+            return find.groupValues[1];
+        }
+        return null
+    }
+
 
 }
 
+data class Hcl(var input: String) {
+
+    fun isValid(): Boolean {
+        return "^#[0-9a-f]{6}\$".toRegex().matches(input)
+    }
+}
+
+data class Pid(var input: String) {
+
+    fun isValid(): Boolean {
+        return "^[0-9]{9}\$".toRegex().matches(input)
+    }
+}
+
+data class Ecl(var input: String) {
+    var validElemts = listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+
+    fun isValid(): Boolean {
+        return validElemts.contains(input)
+    }
+}
+
+data class Hgt(var input: String) {
+    var number: Int = 0
+    var unit: String = ""
+
+    init {
+        val find = "(\\d*)(cm|in)".toRegex().find(input)
+        if (find != null) {
+            number = find.groupValues[1].toInt();
+            unit = find.groupValues[2];
+        }
+
+    }
+
+    fun isValid(): Boolean {
+
+        if (unit == "cm") {
+            return IntRange(150, 193).contains(number)
+        }
+        if (unit == "in") {
+            return IntRange(59, 76).contains(number)
+        }
+        return false
+    }
+}
--- src/test/kotl/de/aoc/day4/Day4Test.kt
+++ src/test/kotl/de/aoc/day4/Day4Test.kt
@@ -1,11 +1,11 @@
 package de.aoc.day4
 
-import de.aoc.day3.Day3Test
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class Day4Test {
-
 
     @Test
     fun day4_a() {
@@ -36,10 +36,107 @@ class Day4Test {
     }
 
     @Test
+    fun day4_b() {
+        val input = Day4Test::class.java.getResource("/day4_input").readText()
+
+        val result = day4_b(input)
+        assertEquals(result, 145)
+    }
+
+    @Test
+    fun day4_b_example_1() {
+        val input = "eyr:1972 cid:100\n" +
+                "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\n" +
+                "\n" +
+                "iyr:2019\n" +
+                "hcl:#602927 eyr:1967 hgt:170cm\n" +
+                "ecl:grn pid:012533040 byr:1946\n" +
+                "\n" +
+                "hcl:dab227 iyr:2012\n" +
+                "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\n" +
+                "\n" +
+                "hgt:59cm ecl:zzz\n" +
+                "eyr:2038 hcl:74454a iyr:2023\n" +
+                "pid:3556412378 byr:2007";
+
+        val result = day4_b(input)
+        assertEquals(result, 0)
+    }
+
+    @Test
+    fun day4_b_example_2() {
+        val input = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\n" +
+                "hcl:#623a2f\n" +
+                "\n" +
+                "eyr:2029 ecl:blu cid:129 byr:1989\n" +
+                "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n" +
+                "\n" +
+                "hcl:#888785\n" +
+                "hgt:164cm byr:2001 iyr:2015 cid:88\n" +
+                "pid:545766238 ecl:hzl\n" +
+                "eyr:2022\n" +
+                "\n" +
+                "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
+
+        val result = day4_b(input)
+        assertEquals(result, 4)
+    }
+
+    @Test
     fun parse() {
         val input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n" +
                 "byr:1937 iyr:2017 cid:147 hgt:183cm\n";
-        assertEquals(Passport(input).components, listOf("ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt"));
+        val passport = Passport(input)
+        assertEquals(passport.ecl?.input, "gry");
+        assertEquals(passport.pid?.input, "860033327");
+        assertEquals(passport.hcl?.input, "#fffffd");
+        assertEquals(passport.byr, 1937);
+        assertEquals(passport.iyr, 2017);
+        assertEquals(passport.hgt?.input, "183cm");
+    }
 
+    @Test
+    fun hcl() {
+        var hcl = Hcl("#123abc")
+        assertTrue(hcl.isValid())
+
+        hcl = Hcl("#123abz")
+        assertFalse(hcl.isValid())
+
+        hcl = Hcl("123abc")
+        assertFalse(hcl.isValid())
+    }
+
+    @Test
+    fun pid() {
+        var pid = Pid("000000001")
+        assertTrue(pid.isValid())
+
+        pid = Pid("0123456789")
+        assertFalse(pid.isValid())
+    }
+
+    @Test
+    fun hgt() {
+        var hgt = Hgt("60in")
+        assertTrue(hgt.isValid())
+
+        hgt = Hgt("190cm")
+        assertTrue(hgt.isValid())
+
+        hgt = Hgt("190in")
+        assertFalse(hgt.isValid())
+
+        hgt = Hgt("190")
+        assertFalse(hgt.isValid())
+    }
+
+    @Test
+    fun ecl() {
+        var ecl = Ecl("brn")
+        assertTrue(ecl.isValid())
+
+        ecl = Ecl("wat")
+        assertFalse(ecl.isValid())
     }
 }
