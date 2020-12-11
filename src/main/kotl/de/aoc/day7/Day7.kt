f1a2c651b432bceeb84277e4c4af964e7811fc76
Julia Burgard <burgard@synyx.de>
2020-12-11 18:44:31 +0100

    Add solution for day 7 part 2

--- src/main/kotl/de/aoc/day7/Day7.kt
+++ src/main/kotl/de/aoc/day7/Day7.kt
@@ -5,23 +5,47 @@ fun day7_a(inputAsString: String): Int {
 
     val bags = inputAsString.split("\n").map { parseBag(it) }
 
-    val bagsContainingGoldBags=ArrayList<Bag>()
-    var bagsToCheck= listOf("shiny gold")
-    while (bagsToCheck.isNotEmpty()){
+    val bagsContainingGoldBags = ArrayList<Bag>()
+    var bagsToCheck = listOf("shiny gold")
+    while (bagsToCheck.isNotEmpty()) {
         val bagsForBagsToCheck = bags.filter { it.containsOneOf(bagsToCheck) }
-        bagsToCheck=bagsForBagsToCheck.map { it.bagname };
+        bagsToCheck = bagsForBagsToCheck.map { it.bagname };
         bagsContainingGoldBags.addAll(bagsForBagsToCheck)
     }
     return bagsContainingGoldBags.distinct().size;
 }
 
-data class Bag(var bagname: String, var containingBags: List<String>){
+fun day7_b(inputAsString: String): Int {
+
+    val bags = inputAsString.split("\n").map { parseBag(it) }
+    val shinyGoldBag = bags.first { it.bagname == "shiny gold" }
+
+    return countBagsInside(shinyGoldBag, bags) - 1;
+}
+
+
+fun countBagsInside(bag: Bag, bags: List<Bag>): Int {
+    if (bag.containingBags.isEmpty()) {
+        return 1
+    }
+
+    val bagsInside = bag.containingBags.map { countBagsInside(get(bags, it.bagname), bags) * it.count }.sum()
+    return bagsInside + 1
+
+}
+
+private fun get(bags: List<Bag>, bagName: String) = bags.first { it.bagname == bagName }
+
+data class Bag(var bagname: String, var containingBags: List<ContainingBag>) {
 
     fun containsOneOf(bagsToContain: List<String>): Boolean {
 
-        return bagsToContain.any{containingBags.contains(it)}
+        return bagsToContain.any { containingBags.map { it.bagname }.contains(it) }
     }
+    
 }
+
+data class ContainingBag(var bagname: String, var count: Int)
 
 fun parseBag(bagNameAsString: String): Bag {
     val bagName = parseBagName(bagNameAsString)
@@ -34,8 +58,8 @@ fun parseBagName(bagAsString: String): String {
     return bagName
 }
 
-fun parseContainingBagNames(bagAsString: String): List<String> {
-    val findAll = "(\\d+ (\\w* \\w*) bag)".toRegex().findAll(bagAsString)
-    return findAll.map { it.groupValues[2] }.toList()
+fun parseContainingBagNames(bagAsString: String): List<ContainingBag> {
+    val findAll = "((\\d+) (\\w* \\w*) bag)".toRegex().findAll(bagAsString)
+    return findAll.map { ContainingBag(it.groupValues[3], it.groupValues[2].toInt()) }.toList()
 }
 
--- src/test/kotl/de/aoc/day7/Day7Test.kt
+++ src/test/kotl/de/aoc/day7/Day7Test.kt
@@ -1,6 +1,8 @@
 package de.aoc.day6
 
+import de.aoc.day7.ContainingBag
 import de.aoc.day7.day7_a
+import de.aoc.day7.day7_b
 import de.aoc.day7.parseBag
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -14,7 +16,7 @@ class Day7Test {
         val input = Day7Test::class.java.getResource("/day7_input").readText()
 
         val result = day7_a(input)
-        assertEquals(192,result)
+        assertEquals(192, result)
     }
 
     @Test
@@ -33,13 +35,50 @@ class Day7Test {
         assertEquals(result, 4)
     }
 
+    @Test
+    fun day7_b() {
+        val input = Day7Test::class.java.getResource("/day7_input").readText()
+
+        val result = day7_b(input)
+        assertEquals(12128, result)
+    }
+
+    @Test
+    fun day7_b_example_1() {
+        val input = "light red bags contain 1 bright white bag, 2 muted yellow bags.\n" +
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" +
+                "bright white bags contain 1 shiny gold bag.\n" +
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" +
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n" +
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n" +
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n" +
+                "faded blue bags contain no other bags.\n" +
+                "dotted black bags contain no other bags."
+
+        val result = day7_b(input)
+        assertEquals(32, result)
+    }
+
+    @Test
+    fun day7_b_example_2() {
+        val input = "shiny gold bags contain 2 dark red bags.\n" +
+                "dark red bags contain 2 dark orange bags.\n" +
+                "dark orange bags contain 2 dark yellow bags.\n" +
+                "dark yellow bags contain 2 dark green bags.\n" +
+                "dark green bags contain 2 dark blue bags.\n" +
+                "dark blue bags contain 2 dark violet bags.\n" +
+                "dark violet bags contain no other bags."
+
+        val result = day7_b(input)
+        assertEquals(126, result)
+    }
 
     @Test
     fun parseBag() {
 
         var bag = parseBag("light red bags contain 1 bright white bag, 2 muted yellow bags.")
         assertEquals("light red", bag.bagname)
-        assertEquals(listOf("bright white", "muted yellow"), bag.containingBags)
+        assertEquals(listOf(ContainingBag("bright white", 1), ContainingBag("muted yellow", 2)), bag.containingBags)
 
         bag = parseBag("dotted black bags contain no other bags.")
         assertEquals("dotted black", bag.bagname)
