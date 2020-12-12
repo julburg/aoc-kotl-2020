57f0898fe4ff7c3fdf1e0be0f3f93d392297722f
Julia Burgard <burgard@synyx.de>
2020-12-12 13:09:47 +0100

    Add solution for day 9 part 2

--- src/main/kotl/de/aoc/day9/Day9.kt
+++ src/main/kotl/de/aoc/day9/Day9.kt
@@ -25,3 +25,35 @@ fun isValid(numberToCheck: Long, lastNumbers: List<Long>): Boolean {
     }
     return false;
 }
+
+fun day9_b(inputAsString: String, numberToCheck: Long): Long {
+    val inputNumbers = inputAsString.split("\n").map { it.toLong() }
+
+    for ((index, _) in inputNumbers.withIndex()) {
+        val encryptionWeakness = getEncryptionWeaknessFor(index, inputNumbers, numberToCheck)
+        if (encryptionWeakness.isNotEmpty()) {
+            val max = encryptionWeakness.maxOrNull()!!
+            val min = encryptionWeakness.minOrNull()!!
+            return max + min
+        }
+    }
+
+    throw Exception("Encryption weakness could not be found.")
+
+}
+
+fun getEncryptionWeaknessFor(index: Int, numbers: List<Long>, numberToCheck: Long): List<Long> {
+    var sum = 0L
+    val allMatchingNumbers = ArrayList<Long>()
+    for (i in index..numbers.size) {
+        if (sum == numberToCheck) {
+            return allMatchingNumbers
+        }
+        if (sum > numberToCheck) {
+            break
+        }
+        allMatchingNumbers.add(numbers[i])
+        sum += numbers[i]
+    }
+    return emptyList()
+}
--- src/test/kotl/de/aoc/day9/Day9Test.kt
+++ src/test/kotl/de/aoc/day9/Day9Test.kt
@@ -44,6 +44,41 @@ class Day9Test {
     }
 
     @Test
+    fun day9b() {
+        val input = Day9Test::class.java.getResource("/day9_input").readText()
+
+        val result = day9_b(input, 29221323)
+        assertEquals(4389369, result)
+    }
+
+    @Test
+    fun day9_b_example_1() {
+        val input = "35\n" +
+                "20\n" +
+                "15\n" +
+                "25\n" +
+                "47\n" +
+                "40\n" +
+                "62\n" +
+                "55\n" +
+                "65\n" +
+                "95\n" +
+                "102\n" +
+                "117\n" +
+                "150\n" +
+                "182\n" +
+                "127\n" +
+                "219\n" +
+                "299\n" +
+                "277\n" +
+                "309\n" +
+                "576"
+
+        val result = day9_b(input, 127)
+        assertEquals(62, result)
+    }
+
+    @Test
     fun isValid_isTrue() {
 
         assertTrue(isValid(40, listOf(35, 20, 15, 25, 47)))
@@ -55,5 +90,13 @@ class Day9Test {
     fun isValid_isFalse() {
 
         assertFalse(isValid(127, listOf(95, 102, 117, 150, 182)))
+    }
+
+    @Test
+    fun encryptionWeakness() {
+
+        assertEquals(getEncryptionWeaknessFor(0, listOf(35, 20, 15, 25, 47, 40), 127), emptyList())
+        assertEquals(getEncryptionWeaknessFor(1, listOf(35, 20, 15, 25, 47, 40), 127), emptyList())
+        assertEquals(listOf(15L, 25L, 47L, 40L), getEncryptionWeaknessFor(2, listOf(35, 20, 15, 25, 47, 40), 127))
     }
 }
