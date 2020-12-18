65e00239bd0aa4028d6b9316256cfc90fd4e123b
Julia Burgard <burgard@synyx.de>
2020-12-18 12:02:35 +0100

    Add day 12 part 2

--- src/main/kotl/de/aoc/day12/Day12.kt
+++ src/main/kotl/de/aoc/day12/Day12.kt
@@ -1,17 +1,33 @@
 package de.aoc.day12
 
+import java.awt.Point
+import kotlin.math.abs
+import kotlin.math.roundToInt
+
 
 fun day12_a(inputAsString: String): Int {
 
     val actions = parseActions(inputAsString)
 
     val ferry = Ferry()
-    for (action in actions){
+    for (action in actions) {
         ferry.execute(action)
     }
-    return Math.abs(ferry.northSouth)+Math.abs(ferry.eastWest)
+    return Math.abs(ferry.northSouth) + Math.abs(ferry.eastWest)
 }
 
+
+fun day12_b(inputAsString: String): Int {
+
+    val actions = parseActions(inputAsString)
+
+    val wayPoint = WayPoint()
+    for (action in actions) {
+        wayPoint.execute(action)
+    }
+
+    return Math.abs(wayPoint.ferry.northSouth) + Math.abs(wayPoint.ferry.eastWest)
+}
 
 fun parseActions(inputAsString: String): ArrayList<Action> {
 
@@ -28,20 +44,77 @@ fun parseActions(inputAsString: String): ArrayList<Action> {
 
 data class Action(val name: String, val value: Int)
 
+open class Moveable(var northSouth: Int, var eastWest: Int) {
 
-class Ferry() {
+}
+
+class WayPoint : Moveable(1, 10) {
+
+    val ferry = Ferry()
+
+    fun execute(action: Action) {
+        when (action.name) {
+            "N" -> move(action.name, action.value, this)
+            "S" -> move(action.name, action.value, this)
+            "E" -> move(action.name, action.value, this)
+            "W" -> move(action.name, action.value, this)
+            "F" -> {
+                moveFerryForwardByWaypoint(action)
+            }
+            "R" -> rotate(action.value)
+            "L" -> rotate(-action.value)
+
+            else -> {
+                throw Exception("Action $action not implemented")
+            }
+        }
+
+    }
+
+    fun moveFerryForwardByWaypoint(action: Action) {
+        move(getDiectionNorthSouth().shortName, abs(northSouth) * action.value, ferry)
+        move(getDiectionEastWest().shortName, abs(eastWest) * action.value, ferry)
+    }
+
+    fun getDiectionNorthSouth(): Direction {
+        if (northSouth > 0) {
+            return Direction.NORTH
+        }
+        return Direction.SOUTH
+    }
+
+    fun getDiectionEastWest(): Direction {
+        if (eastWest > 0) {
+            return Direction.EAST
+        }
+        return Direction.WEST
+    }
+
+    fun rotate(degrees: Int) {
+        val point = Point(northSouth, eastWest)
+
+        //copied from stackoverflow
+        val angle = Math.toRadians(degrees.toDouble())
+        val x2 = point.x * Math.cos(angle) - point.y * Math.sin(angle)
+        val y2 = point.x * Math.sin(angle) + point.y * Math.cos(angle)
+
+        northSouth = x2.roundToInt()
+        eastWest = y2.roundToInt()
+    }
+
+}
+
+class Ferry : Moveable(0, 0) {
     var facingDirection = Direction.EAST
-    var northSouth = 0
-    var eastWest = 0
 
 
     fun execute(action: Action) {
         when (action.name) {
-            "N" -> move(action.name, action.value)
-            "S" -> move(action.name, action.value)
-            "E" -> move(action.name, action.value)
-            "W" -> move(action.name, action.value)
-            "F" -> move(facingDirection.shortName, action.value)
+            "N" -> move(action.name, action.value, this)
+            "S" -> move(action.name, action.value, this)
+            "E" -> move(action.name, action.value, this)
+            "W" -> move(action.name, action.value, this)
+            "F" -> move(facingDirection.shortName, action.value, this)
             "R" -> turn(action.name, action.value)
             "L" -> turn(action.name, action.value)
             else -> {
@@ -51,17 +124,6 @@ class Ferry() {
 
     }
 
-    private fun move(action: String, value: Int) {
-        when (action) {
-            "N" -> northSouth = northSouth + value
-            "S" -> northSouth = northSouth - value
-            "E" -> eastWest = eastWest + value
-            "W" -> eastWest = eastWest - value
-            else -> {
-                throw Exception("Action $action not implemented")
-            }
-        }
-    }
 
     fun turn(direction: String, degrees: Int) {
         val steps = degrees / 90
@@ -74,7 +136,22 @@ class Ferry() {
             }
         }
     }
+
+
 }
+
+private fun move(action: String, value: Int, moveabale: Moveable) {
+    when (action) {
+        "N" -> moveabale.northSouth = moveabale.northSouth + value
+        "S" -> moveabale.northSouth = moveabale.northSouth - value
+        "E" -> moveabale.eastWest = moveabale.eastWest + value
+        "W" -> moveabale.eastWest = moveabale.eastWest - value
+        else -> {
+            throw Exception("Action $action not implemented")
+        }
+    }
+}
+
 
 enum class Direction(var shortName: String) {
     NORTH("N"), EAST("E"), SOUTH("S"), WEST("W")
--- src/test/kotl/de/aoc/day12/Day12Test.kt
+++ src/test/kotl/de/aoc/day12/Day12Test.kt
@@ -28,6 +28,26 @@ class Day12Test {
         assertEquals(25, result)
     }
 
+    @Test
+    fun day12_b() {
+        val input = Day12Test::class.java.getResource("/day12_input").readText()
+
+        val result = day12_b(input)
+        assertEquals(27016, result)
+    }
+
+    @Test
+    fun day12_b_example_1() {
+        val input = "F10\n" +
+                "N3\n" +
+                "F7\n" +
+                "R90\n" +
+                "F11"
+
+        val result = day12_b(input)
+        assertEquals(286, result)
+    }
+
 
     @Test
     internal fun parseActions() {
@@ -86,5 +106,55 @@ class Day12Test {
 
         ferry.execute(Action("F", 5))
         assertEquals(ferry.northSouth, 5)
+    }
+
+    @Test
+    internal fun moveFerryForwardByWaypoint_northSouth() {
+        val wayPoint = WayPoint();
+        wayPoint.execute(Action("F", 10))
+
+        assertEquals(wayPoint.ferry.northSouth, 10)
+        assertEquals(wayPoint.ferry.eastWest, 100)
+    }
+
+    @Test
+    internal fun moveFerryForwardByWaypoint_eastWest() {
+        val wayPoint = WayPoint();
+        wayPoint.eastWest = -10
+        wayPoint.northSouth = -1
+
+        wayPoint.execute(Action("F", 10))
+
+        assertEquals(wayPoint.ferry.northSouth, -10)
+        assertEquals(wayPoint.ferry.eastWest, -100)
+    }
+
+
+    @Test
+    internal fun rotate() {
+
+        val wayPoint = WayPoint();
+        wayPoint.northSouth = 4
+        wayPoint.eastWest = 10
+
+        wayPoint.rotate(90)
+
+        assertEquals(wayPoint.northSouth, -10)
+        assertEquals(wayPoint.eastWest, 4)
+    }
+
+    @Test
+    internal fun rotate_easy() {
+
+        val wayPoint = WayPoint();
+        wayPoint.northSouth = 1
+        wayPoint.eastWest = 1
+        wayPoint.ferry.northSouth = 0
+        wayPoint.ferry.eastWest = 0
+
+        wayPoint.rotate(90)
+
+        assertEquals(wayPoint.northSouth, -1)
+        assertEquals(wayPoint.eastWest, 1)
     }
 }
